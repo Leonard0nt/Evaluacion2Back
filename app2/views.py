088b32a1f3ccf7cff7e9d4ec4dca062956de1308
LeopoldoWall <leopoldo.wall@inacapmail.cl>
2023-10-19 09:34:38 -0300

    vista america agregada

--- app2/views.py
+++ app2/views.py
@@ -7,3 +7,11 @@ def renderTemplate2(request):
 def renderAsia(request):
     datos = {"nombre": "Asia", "monumento1": "Gran Buda", "monumento2": "Gateaway of india", "monumento3":"Shwedagon Paya", "imagen1":"../static/images/Asia/Asia1.jpg", "imagen2":"../static/images/Asia/Asia2.jpg","imagen3": "../static/images/Asia/Asia3.jpg" }
     return render(request, 'templatesApp/index2.html',datos)
+
+def renderAmerica(request):
+    datos = {"nombre": "America", "monumento1": "Cristo Redentor", "monumento2": "Moai", "monumento3":"Monte RushMore", "imagen1":"../static/images/America/America1.jpg", "imagen2":"../static/images/America/America2.jpg","imagen3": "../static/images/America/America3.jpg" }
+    return render(request, 'templatesApp/index2.html',datos)
+
+def renderAsia(request):
+    datos = {"nombre": "Europa", "monumento1": "Torre Eifel", "monumento2": "Torre de Pizza", "monumento3":"Coliseo Romano", "imagen1":"../static/images/Europa/Europa1.jpg", "imagen2":"../static/images/Europa/Europa2.jpg","imagen3": "../static/images/Europa/Europa3.jpg" }
+    return render(request, 'templatesApp/index2.html',datos)
